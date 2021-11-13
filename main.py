6d7c6d49aa55e735b343da887eaedecff856bfcb
Erick Josue Pulla Zambrano <epulla@espol.edu.ec>
2021-11-13 15:43:18 -0500

    Main script with file input argument working

--- main.py
+++ main.py
@@ -2,18 +2,30 @@
 # By Erick Pulla
 # Made using python3
 
-from util import Schedule, get_unique_pairs
+import sys
 
-schedules = []
-with open("data.txt", "r") as f:
-    for line in f:
-        name, data = line.strip().split("=")
-        schedules.append(Schedule(name, data))
+def main(argv):
+    """Main function of the program
+    """
+    
+    if len(argv) != 2:
+        print("Usage:\n(Windows) python main.py <inputfile>\n(Linux/Mac) python3 main.py <inputfile>")
+        sys.exit()
+    
+    from util import Schedule, get_unique_pairs
 
-unique_pairs = get_unique_pairs(schedules)
+    schedules = []
+    with open(argv[1], "r") as f:
+        for line in f:
+            name, data = line.strip().split("=")
+            schedules.append(Schedule(name, data))
 
-for schedule1, schedule2 in unique_pairs:
-    coincidences = schedule1.compare(schedule2)
-    if coincidences > 0:
-        print("%s-%s: %d"%(schedule1.owner, schedule2.owner, coincidences))
-        
+    unique_pairs = get_unique_pairs(schedules)
+
+    for schedule1, schedule2 in unique_pairs:
+        coincidences = schedule1.compare(schedule2)
+        if coincidences > 0:
+            print("%s-%s: %d"%(schedule1.owner, schedule2.owner, coincidences))
+
+if __name__ == "__main__":
+    main(sys.argv)
